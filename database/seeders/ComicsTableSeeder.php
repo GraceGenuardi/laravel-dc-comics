7cac02a7044fd20c32211d13f03b5ed3386bda6c
GraceGenuardi <graziagenuardi96@gmail.com>
2023-04-15 11:15:06 +0200

    EDIT TAB SEEDER POPOLATA

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -14,6 +14,19 @@ class ComicsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
-    }
+
+        //Tabella Seeder
+        $comics = config('comics');
+          foreach ($comics as $comic) {
+            $newComic = new Comic();
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+            $newComic->save();
+        };
+    };
 }
