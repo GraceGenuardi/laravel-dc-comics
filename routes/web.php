8fcacd74ef8e047bb3bd22b18bff9e198b4a3dec
GraceGenuardi <graziagenuardi96@gmail.com>
2023-04-17 14:10:48 +0200

    FEAT commentate rotte con spiegazione

--- routes/web.php
+++ routes/web.php
@@ -25,13 +25,13 @@ Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
 
 
 //ROTTE PARTE 2: CREATE, STORE, EDIT, UPDATE
-Route::get('/products/create', 'ProductController@create')->name('products.create');
+Route::get('/products/create', 'ProductController@create')->name('products.create'); // crea 
 
-Route::post('/products', 'ProductController@store')->name('products.store');
+Route::post('/products', 'ProductController@store')->name('products.store');// salva
 
-Route::get('/products/{id}/edit', 'ProductController@edit')->name('products.edit');
+Route::get('/products/{id}/edit', 'ProductController@edit')->name('products.edit');// modifica 
 
-Route::put('/products/{id}', 'ProductController@update')->name('products.update');
+Route::put('/products/{id}', 'ProductController@update')->name('products.update');// aggiorna
 
 
 
